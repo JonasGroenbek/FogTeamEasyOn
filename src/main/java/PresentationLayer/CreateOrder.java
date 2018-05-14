d16ba85b80db3cbec3992edf6a1e32d17a4db18e
jonas <jonasgroenbek@gmail.com>
2018-05-14 20:10:38 +0200

    løst merge konflikt

--- src/main/java/PresentationLayer/CreateOrder.java
+++ src/main/java/PresentationLayer/CreateOrder.java
@@ -10,32 +10,31 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class CreateOrder extends Command {
-
+    
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession();        
         User user = (User) session.getAttribute("user");
         String email = user.getEmail();
         int material = Integer.parseInt(request.getParameter("material"));
         int roof = Integer.parseInt(request.getParameter("roof"));
         int lengthShed = Integer.parseInt(request.getParameter("lengthShed"));
         int widthShed = Integer.parseInt(request.getParameter("widthShed"));
-        int length = Integer.parseInt(request.getParameter("length")) * 100;
-        int width = Integer.parseInt(request.getParameter("width")) * 100;
-        int height = Integer.parseInt(request.getParameter("height"));
-        SVGBuilder svg = new SVGBuilder(roof, length, width, lengthShed, widthShed, material);
-
-        Order order = new Order(length, width, height);
+        int length = Integer.parseInt(request.getParameter("length"));
+        int width = Integer.parseInt(request.getParameter("width"));
+        int height = 300;
+        int assemble = (request.getParameter("assemble") == null) ? 0 : 1;
+        
         Shed shed = new Shed(lengthShed, widthShed, 500);
-        session.setAttribute("svg", svg.buildSvg(svg)); 
+        Order order = new Order(length, width, height);
         
         session.setAttribute("length", length);
         session.setAttribute("width", width);
-
+        
         LogicFacade.createShed(shed, email);
-        LogicFacade.createOrder(user.getEmail(), 500, order, material, roof, shed);
-
+        LogicFacade.createOrder(user.getEmail(), 500, order, material, roof, shed, assemble);
+        
         return "placedorderpage";
-
+        
     }
 }
