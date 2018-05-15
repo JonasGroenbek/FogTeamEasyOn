6fbee542c34bc7c1aca33b1b012bbba015f0c901
jonas <jonasgroenbek@gmail.com>
2018-05-15 21:32:09 +0200

    renset SVG-klassen, samt lavet stolper, både med og uden skur og bind til en carport uden skur

--- src/main/java/PresentationLayer/CreateOrder.java
+++ src/main/java/PresentationLayer/CreateOrder.java
@@ -10,10 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class CreateOrder extends Command {
-    
+
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
-        HttpSession session = request.getSession();        
+        HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
         int userID = user.getId();
         int material = Integer.parseInt(request.getParameter("material"));
@@ -23,16 +23,17 @@ public class CreateOrder extends Command {
         int length = Integer.parseInt(request.getParameter("length"));
         int width = Integer.parseInt(request.getParameter("width"));
         int height = 300;
-        SVGBuilder svg = new SVGBuilder(roof, length, width, lengthShed, widthShed, material);
-        session.setAttribute("svg", svg.buildSvg(svg));
         
         Shed shed = new Shed(lengthShed, widthShed, 500);
+        SVGBuilder svg = new SVGBuilder(roof, length, width, shed, material);
+        session.setAttribute("svg", svg.buildSvg(svg));
+
         Order order = new Order(length, width, height);
-        
+
         LogicFacade.createShed(shed, userID);
         LogicFacade.createOrder(userID, 500, order, material, roof, shed);
-        
+
         return "placedorderpage";
-        
+
     }
 }
--- src/main/java/PresentationLayer/SVGBuilder.java
+++ src/main/java/PresentationLayer/SVGBuilder.java
@@ -1,100 +1,199 @@
 package PresentationLayer;
 
+import FunctionLayer.Shed;
+import java.text.DecimalFormat;
+
 public class SVGBuilder {
 
-    StringBuilder sb = new StringBuilder();
-    int roofType;
-    int length;
-    int width;
-    int lengthShed;
-    int widthShed;
-    int material;
+    private StringBuilder sb = new StringBuilder();
+    private final int roofType;
+    private final int length;
+    private final int width;
+    private final Shed shed;
+    private final int material; //will be used later
+    private final int firstX = 80; //if below 45 then the text for rafterspacers will be messed up
+    private final int firstY = 90;
+    private final int roofOverhang = 35;
+    final double rafter = 4.5;
+    private double spacing;
 
-    public SVGBuilder(int roofType, int length, int width, int lengthShed, int widthShed, int material) {
+    public SVGBuilder(int roofType, int length, int width, Shed shed, int material) {
         this.roofType = roofType;
         this.length = length;
         this.width = width;
-        this.lengthShed = lengthShed;
-        this.widthShed = widthShed;
+        this.shed = shed;
         this.material = material;
+    }
+
+    private int getMaxY(SVGBuilder svg) {
+        return svg.length + firstY;
+    }
+
+    private int getMaxX(SVGBuilder svg) {
+        return svg.width + firstX;
     }
 
     // x er width, y er length
     public String buildSvg(SVGBuilder svg) {
-        int viewboxWidth = svg.width + 150;
-        int viewboxLength = svg.length + 150;
+        int viewboxWidth = svg.width + firstX + 150;
+        int viewboxLength = svg.length + firstY + 150;
         sb.append("<svg width=\"750\" height=\"750\" viewBox=\"0 0 ").append(viewboxWidth).append(" ").append(viewboxLength).append("\">\n");
         sb.append(buildRectangle(svg));
         sb.append(buildYArrow(svg));
         sb.append(buildXArrow(svg));
         buildRafter(svg);
+        buildRem(svg);
+        createShed(svg);
+        createPosts(svg);
         sb.append("\n</svg>");
         String res = sb.toString();
-
         return res;
     }
 
     private String buildRectangle(SVGBuilder svg) {
-        String res = " <rect x=\"20\" y=\"20\" width=\"" + svg.width + "\""
+        String res = " <rect x=\"" + firstX + "\" y=\"" + firstY + "\" width=\"" + svg.width + "\""
                 + "height=\"" + svg.length + "\""
                 + "style=\"fill:white;stroke:black;stroke-width:3;\"/>\n";
         return res;
     }
 
+    //The line going vertical
     private String buildYArrow(SVGBuilder svg) {
-        int x = svg.width + 50;
-        int y2 = svg.length + 20;
-        String line = "<line x1=\"" + x + "\" y1=\"20\" x2=\"" + x + "\" y2=\"" + y2
+        int x = svg.width + firstX + 30; //it has to be a bit more to the right than the house to be visible.
+        int y2 = svg.length + firstY;
+        String line = "<line x1=\"" + x + "\" y1=\"" + firstY + "\" x2=\"" + x + "\" y2=\"" + y2
                 + "\" stroke=\"#000\" stroke-width=\"2\"/> \n";
         int textX = x + 10;
-        int textY = y2 / 2 - 10;
+        int textY = svg.length / 2 + firstY;
         String res = line.concat("<text x=\"" + textX + "\" y=\"" + textY + "\" font-family=\"Verdana\" font-size=\"15\">\n"
                 + " " + svg.length + "cm\n </text>");
         return res;
     }
 
     private String buildXArrow(SVGBuilder svg) {
-        int x2 = svg.width + 20;
-        int y = svg.length + 50;
-        String line = "<line x1=\"20\" y1=\"" + y + "\" x2=\"" + x2 + "\" y2=\"" + y
+        int x2 = svg.width + firstX;
+        int y = svg.length + firstY + 30; // it has to be a bit lower than the house to be visible.
+        String line = "<line x1=\"" + firstX + "\" y1=\"" + y + "\" x2=\"" + x2 + "\" y2=\"" + y
                 + "\" stroke=\"#000\" stroke-width=\"2\"/> \n";
-        int textX = x2 / 2 - 10;
-        int textY = y + 15;
-        String res = line.concat("<text x=\"" + textX + "\" y=\"" + textY + "\" font-family=\"Verdana\" font-size=\"15\">\n"
-                + " " + svg.width + "cm\n </text>");
+        int textX = x2 / 2 - 10; //in the middle, and -10 more for the text to be centered
+        int textY = y + 15; //a bit underneath the line
+        String res = line.concat("<text x=\"" + textX + "\" y=\"" + textY + "\" font-family=\"Verdana\" font-size=\"15\">\n" + svg.width + "cm\n </text>");
         return res;
     }
 
     private void buildRafter(SVGBuilder svg) { //spær
-        final double rafter = 4.5;
-        final int y1 = 20;
-        final int y2 = svg.length + 20;
-        double x = 24;
-        final int xe = svg.width + 16;
-        double spacing = calcSpacing(svg.width);
-        
-        
-        sb.append("<line x1=\"" + x + "\" y1=\"" + y1 + "\" x2=\"" + x + "\" y2=\"" + y2
+        final int y2 = svg.length + firstY;
+        double x = firstX + rafter; //this is the leftmost rafter which has to be placed before any others
+        double xe = svg.width + firstX - rafter; //this is the rightmost rafter which has to be placed before any others
+        spacing = calcSpacing(svg.width);
+
+        sb.append("<line x1=\"" + x + "\" y1=\"" + firstY + "\" x2=\"" + x + "\" y2=\"" + y2
                 + "\" stroke=\"#000\" stroke-width=\"2\"/> \n");
-        sb.append("<line x1=\"" + xe + "\" y1=\"" + y1 + "\" x2=\"" + xe + "\" y2=\"" + y2
+        sb.append("<line x1=\"" + xe + "\" y1=\"" + firstY + "\" x2=\"" + xe + "\" y2=\"" + y2
                 + "\" stroke=\"#000\" stroke-width=\"2\"/> \n");
-   
-        for (double i = spacing*2+4.5; i < svg.width-9; i+= spacing) {
-            x+=spacing;
-            sb.append("<line x1=\"" + x + "\" y1=\"" + y1 + "\" x2=\"" + x + "\" y2=\"" + y2
+
+        for (double i = spacing * 2 + rafter; i < svg.width - (rafter * 2); i += spacing) {
+            x += spacing;
+            sb.append("<line x1=\"" + x + "\" y1=\"" + firstY + "\" x2=\"" + x + "\" y2=\"" + y2
                     + "\" stroke=\"#000\" stroke-width=\"2\"/> \n");
-            x+=4.5;
-            sb.append("<line x1=\"" + x + "\" y1=\"" + y1 + "\" x2=\"" + x + "\" y2=\"" + y2
+            x += 4.5;
+            sb.append("<line x1=\"" + x + "\" y1=\"" + firstY + "\" x2=\"" + x + "\" y2=\"" + y2
                     + "\" stroke=\"#000\" stroke-width=\"2\"/> \n");
 
         }
-    }
-    
-    private double calcSpacing(int width){
-        //9 represents the size of the two outer rafters, which has to be considered 
-        //to get a precise spacing.
-        int amountOfRafters = (int) Math.ceil((width-9) / 64.5);
-        return (width-amountOfRafters*4.5) / amountOfRafters;
+        spacingMarkers(spacing, svg);
     }
 
+    private double calcSpacing(int width) {
+        //first the number of rafters needed if the optimal spacing is used. If it is
+        //a decimal number, it gets rounded up to find the amount closest to the optimal amount.
+        int amountOfRafters = (int) Math.ceil((width - (rafter * 2)) / (60 + rafter));
+        //since the amountofrafters
+        return ((width - rafter) - amountOfRafters * rafter) / amountOfRafters;
+    }
+
+    private void spacingMarkers(double spacing, SVGBuilder svg) {
+        for (double i = 0 + spacing; i < svg.width; i += spacing + rafter) {
+            DecimalFormat numberFormat = new DecimalFormat("#.00");
+            int textPlacementY = firstY - 20;
+            double textPlacement = i + firstX - 45;
+            sb.append("<text x=\"" + textPlacement + "\" y=\"" + textPlacementY + "\" font-family=\"Verdana\" font-size=\"10\">\n"
+                    + " " + numberFormat.format(spacing) + "cm\n </text>");
+        }
+    }
+
+    private void buildRem(SVGBuilder svg) {
+        int y = firstY + roofOverhang;
+        int maxY = svg.getMaxY(svg) - (roofOverhang + 5);
+        //int maxX = svg.getMaxX(svg);
+        int textPlacementXtop = firstX - 40;
+        int textPlacementYtop = firstY + roofOverhang / 2;
+        int textPlacementYbot = svg.getMaxY(svg) - roofOverhang/2;
+        int textPlacementXbot = firstX - 40;
+
+        sb.append("<rect x=\"" + firstX + "\" y=\"" + y + "\" width=\"" + svg.width + "\" height=\"4\"/>");
+
+        sb.append("<rect x=\"" + firstX + "\" y=\"" + maxY + "\" width=\"" + svg.width + "\" height=\"4\"/>");
+
+        sb.append("<line x1=\"" + firstX + "\" y1=\"" + firstY + "\" x2=\"" + firstX + "\" y2=\"" + y + "\" stroke=\"#000\" stroke-width=\"2\"/> ");
+        sb.append("<text x=\"" + textPlacementXtop + "\" y=\"" + textPlacementYtop + "\" font-family=\"Verdana\" font-size=\"10\"> 35cm </text>\"");      
+        sb.append("<text x=\"" + textPlacementXbot + "\" y=\"" + textPlacementYbot + "\" font-family=\"Verdana\" font-size=\"10\"> 35cm </text>\"");
+    }
+
+    private void createShed(SVGBuilder svg) {
+        int y = firstY + roofOverhang;
+        sb.append("<rect x=\"" + firstX + "\" y=\"" + y + "\" width=\"" + svg.shed.getWidth() + "\" "
+                + "height=\"" + svg.shed.getLength() + "\" stroke-width= \"4\"; stroke=\"black\" fill= \"none\";/>");
+    }
+
+    private void createPosts(SVGBuilder svg) {
+        int middleOfY = firstY + svg.length / 2;
+        int middleOfX = firstX + svg.width / 2;
+        int minYPole = firstY + roofOverhang;
+        int maxYPole = svg.getMaxY(svg) - roofOverhang;
+        double minXPlusSpacing = firstX + spacing + rafter;
+        double maxXMinusSpacing = svg.getMaxX(svg) - (spacing + rafter);
+
+        if (svg.shed.getLength() > 150) {
+            int shedWidth = svg.shed.getWidth() + firstX;
+            int shedLength = svg.shed.getLength() + firstY + roofOverhang;
+            int middleOfShedX = firstX + svg.shed.getWidth() / 2;
+
+            sb.append("<circle cx=\"" + firstX + "\" cy=\"" + minYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + firstX + "\" cy=\"" + shedLength + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + shedWidth + "\" cy=\"" + minYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + shedWidth + "\" cy=\"" + shedLength + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + middleOfShedX + "\" cy=\"" + minYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + middleOfShedX + "\" cy=\"" + shedLength + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+
+            sb.append("<circle cx=\"" + firstX + "\" cy=\"" + maxYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + svg.getMaxX(svg) + "\" cy=\"" + maxYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + svg.getMaxX(svg) + "\" cy=\"" + minYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+
+            if (svg.width > 500) {
+                sb.append("<circle cx=\"" + middleOfX + "\" cy=\"" + maxYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            }
+            if (svg.length > 500) {
+                sb.append("<circle cx=\"" + svg.getMaxX(svg) + "\" cy=\"" + middleOfY + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            }
+        } else { //setting up the poles markers, without a shed.
+            sb.append("<circle cx=\"" + firstX + "\" cy=\"" + maxYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + firstX + "\" cy=\"" + minYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + svg.getMaxX(svg) + "\" cy=\"" + maxYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            sb.append("<circle cx=\"" + svg.getMaxX(svg) + "\" cy=\"" + minYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            //creating 
+            sb.append("<line stroke-dasharray=\"5, 5, 1, 5\"  x1=\"" + minXPlusSpacing + "\" y1=\"" + minYPole + "\" x2=\"" + maxXMinusSpacing + "\" y2=\"" + maxYPole
+                    + "\" stroke=\"#000\" stroke-width=\"2\"/> \n");
+            sb.append("<line stroke-dasharray=\"5, 5, 1, 5\"  x1=\"" + maxXMinusSpacing + "\" y1=\"" + minYPole + "\" x2=\"" + minXPlusSpacing + "\" y2=\"" + maxYPole
+                    + "\" stroke=\"#000\" stroke-width=\"2\"/> \n");
+            if (svg.width > 500) {
+                sb.append("<circle cx=\"" + middleOfX + "\" cy=\"" + maxYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+                sb.append("<circle cx=\"" + middleOfX + "\" cy=\"" + minYPole + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            }
+            if (svg.length > 500) {
+                sb.append("<circle cx=\"" + svg.getMaxX(svg) + "\" cy=\"" + middleOfY + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+                sb.append("<circle cx=\"" + firstX + "\" cy=\"" + middleOfY + "\" r=\"4\" stroke=\"black\" stroke-width=\"1\" fill=\"white\"/>");
+            }
+        }
+    }
 }
