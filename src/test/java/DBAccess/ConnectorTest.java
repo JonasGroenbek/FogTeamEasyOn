c6c69caa20fff99231f491cfa560741fc445c21b
sebbelebbe <sebbelebbe8930@gmail.com>
2018-05-15 13:44:33 +0200

    testpack

--- src/test/java/DBAccess/ConnectorTest.java
+++ src/test/java/DBAccess/ConnectorTest.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DBAccess;
-
 import java.sql.Connection;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -37,30 +31,10 @@ public class ConnectorTest {
     @After
     public void tearDown() {
     }
-
-    /**
-     * Test of setConnection method, of class Connector.
-     */
-    @Test
-    public void testSetConnection() {
-        System.out.println("setConnection");
-        Connection con = null;
-        Connector.setConnection(con);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of connection method, of class Connector.
-     */
     @Test
     public void testConnection() throws Exception {
-        System.out.println("connection");
-        Connection expResult = null;
-        Connection result = Connector.connection();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        assertTrue(Connector.connection() != null);
     }
     
 }
