95fc0a231ed0c66fdd718efd4b19699ac02df424
sebbelebbe <sebbelebbe8930@gmail.com>
2018-05-23 10:08:45 +0200

    img

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,10 +8,17 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Welcome page</title>
     </head>
     <body>
+<div class="jumbotron">
+    <img src="/images/fogcccc.jpg">
+</div>
         <h1>Welcome to Sem 2</h1>
 
         <table>
--- src/test/java/FunctionLayer/BillCalcTest.java
+++ src/test/java/FunctionLayer/BillCalcTest.java
@@ -73,7 +73,7 @@ public class BillCalcTest {
         System.out.println("overSternBoardFront");
         int length = 600;
         BillCalc instance = new BillCalc();
-        int expResult = 2;
+        int expResult = 4;
         int result = instance.overSternBoardFront(length);
         assertEquals(expResult, result);
 
@@ -87,7 +87,7 @@ public class BillCalcTest {
         System.out.println("overSternBoardSides");
         int length = 600;
         BillCalc instance = new BillCalc();
-        int expResult = 4;
+        int expResult = 2;
         int result = instance.overSternBoardSides(length);
         assertEquals(expResult, result);
 
@@ -116,7 +116,7 @@ public class BillCalcTest {
         System.out.println("interTiesShedGabled");
         int shedl = 530;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 12;
         int result = instance.interTiesShedGabled(shedl);
         assertEquals(expResult, result);
 
@@ -128,9 +128,9 @@ public class BillCalcTest {
     @Test
     public void testInterTiesChedSides() {
         System.out.println("interTiesChedSides");
-        int shedw = 0;
+        int shedw = 120;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 4;
         int result = instance.interTiesChedSides(shedw);
         assertEquals(expResult, result);
 
@@ -142,10 +142,10 @@ public class BillCalcTest {
     @Test
     public void testRemsOnSides() {
         System.out.println("remsOnSides");
-        int width = 0;
-        int shedl = 0;
+        int width = 780;
+        int shedl = 120;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 2;
         int result = instance.remsOnSides(width, shedl);
         assertEquals(expResult, result);
 
@@ -157,9 +157,9 @@ public class BillCalcTest {
     @Test
     public void testRemsOnShed() {
         System.out.println("remsOnShed");
-        int shedWidth = 0;
+        int shedWidth = 120;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.remsOnShed(shedWidth);
         assertEquals(expResult, result);
 
@@ -171,10 +171,10 @@ public class BillCalcTest {
     @Test
     public void testRafter() {
         System.out.println("rafter");
-        int width = 0;
-        int length = 0;
+        int width = 780;
+        int length = 600;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 15;
         int result = instance.rafter(width, length);
         assertEquals(expResult, result);
 
@@ -186,11 +186,11 @@ public class BillCalcTest {
     @Test
     public void testPosts() {
         System.out.println("posts");
-        int width = 0;
-        int length = 0;
-        int shedl = 0;
+        int width = 780;
+        int length = 600;
+        int shedl = 120;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 11;
         int result = instance.posts(width, length, shedl);
         assertEquals(expResult, result);
 
@@ -202,10 +202,10 @@ public class BillCalcTest {
     @Test
     public void testCladding() {
         System.out.println("cladding");
-        int shedWidth = 0;
-        int shedLength = 0;
+        int shedWidth = 530;
+        int shedLength = 120;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 200;
         int result = instance.cladding(shedWidth, shedLength);
         assertEquals(expResult, result);
 
@@ -219,7 +219,7 @@ public class BillCalcTest {
         System.out.println("vandbraetOnSternSides");
         int overSternBoardsSides = 0;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 4;
         int result = instance.vandbraetOnSternSides(overSternBoardsSides);
         assertEquals(expResult, result);
 
@@ -231,9 +231,9 @@ public class BillCalcTest {
     @Test
     public void testVandbraetOnSternFront() {
         System.out.println("vandbraetOnSternFront");
-        int overSternBoardsFront = 0;
+        int overSternBoardsFront = 2;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 2;
         int result = instance.vandbraetOnSternFront(overSternBoardsFront);
         assertEquals(expResult, result);
 
@@ -245,9 +245,9 @@ public class BillCalcTest {
     @Test
     public void testBigRoofPlates() {
         System.out.println("bigRoofPlates");
-        int length = 0;
+        int length = 600;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 6;
         int result = instance.bigRoofPlates(length);
         assertEquals(expResult, result);
 
@@ -259,9 +259,9 @@ public class BillCalcTest {
     @Test
     public void testSmallRoofPlates() {
         System.out.println("SmallRoofPlates");
-        int length = 0;
+        int length = 600;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 6;
         int result = instance.SmallRoofPlates(length);
         assertEquals(expResult, result);
 
@@ -275,7 +275,7 @@ public class BillCalcTest {
         System.out.println("roofScrews");
         int roofplates = 0;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 3;
         int result = instance.roofScrews(roofplates);
         assertEquals(expResult, result);
 
@@ -288,7 +288,7 @@ public class BillCalcTest {
     public void testHoleband() {
         System.out.println("holeband");
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 2;
         int result = instance.holeband();
         assertEquals(expResult, result);
 
@@ -300,9 +300,9 @@ public class BillCalcTest {
     @Test
     public void testRafterBracketRight() {
         System.out.println("rafterBracketRight");
-        int rafters = 0;
+        int rafters = 15;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 15;
         int result = instance.rafterBracketRight(rafters);
         assertEquals(expResult, result);
 
@@ -314,9 +314,9 @@ public class BillCalcTest {
     @Test
     public void testRafterBracketLeft() {
         System.out.println("rafterBracketLeft");
-        int rafters = 0;
+        int rafters = 15;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 15;
         int result = instance.rafterBracketLeft(rafters);
         assertEquals(expResult, result);
       
@@ -328,12 +328,12 @@ public class BillCalcTest {
     @Test
     public void testScrewsVandbraetAndStern() {
         System.out.println("screwsVandbraetAndStern");
-        int underSternFrontAndBack = 0;
-        int underSternSides = 0;
-        int overSternFront = 0;
-        int overSternSides = 0;
+        int underSternFrontAndBack = 4;
+        int underSternSides = 4;
+        int overSternFront = 2;
+        int overSternSides = 4;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.screwsVandbraetAndStern(underSternFrontAndBack, underSternSides, overSternFront, overSternSides);
         assertEquals(expResult, result);
 
@@ -345,9 +345,9 @@ public class BillCalcTest {
     @Test
     public void testScrewsForUniAndBand() {
         System.out.println("screwsForUniAndBand");
-        int uniBrackets = 0;
+        int uniBrackets = 30;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 3;
         int result = instance.screwsForUniAndBand(uniBrackets);
         assertEquals(expResult, result);
 
@@ -359,9 +359,9 @@ public class BillCalcTest {
     @Test
     public void testCarriageBolt() {
         System.out.println("carriageBolt");
-        int posts = 0;
+        int posts = 11;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 18;
         int result = instance.carriageBolt(posts);
         assertEquals(expResult, result);
 
@@ -373,9 +373,9 @@ public class BillCalcTest {
     @Test
     public void testSquareSlices() {
         System.out.println("squareSlices");
-        int posts = 0;
+        int posts = 11;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 12;
         int result = instance.squareSlices(posts);
         assertEquals(expResult, result);
 
@@ -387,9 +387,9 @@ public class BillCalcTest {
     @Test
     public void testCladdingScrewsExternal() {
         System.out.println("claddingScrewsExternal");
-        int cladding = 0;
+        int cladding = 200;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 2;
         int result = instance.claddingScrewsExternal(cladding);
         assertEquals(expResult, result);
 
@@ -401,9 +401,9 @@ public class BillCalcTest {
     @Test
     public void testCladdingScrewsInternal() {
         System.out.println("claddingScrewsInternal");
-        int cladding = 0;
+        int cladding = 200;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 2;
         int result = instance.claddingScrewsInternal(cladding);
         assertEquals(expResult, result);
 
@@ -415,10 +415,10 @@ public class BillCalcTest {
     @Test
     public void testHandle() {
         System.out.println("handle");
-        int shedw = 0;
-        int shedl = 0;
+        int shedw = 120;
+        int shedl = 530;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 1;
         int result = instance.handle(shedw, shedl);
         assertEquals(expResult, result);
 
@@ -430,10 +430,10 @@ public class BillCalcTest {
     @Test
     public void testTHinge() {
         System.out.println("tHinge");
-        int shedw = 0;
-        int shedl = 0;
+        int shedw = 120;
+        int shedl = 530;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 2;
         int result = instance.tHinge(shedw, shedl);
         assertEquals(expResult, result);
 
@@ -445,10 +445,10 @@ public class BillCalcTest {
     @Test
     public void testAngleBrackets() {
         System.out.println("angleBrackets");
-        int interTiesGabel = 0;
-        int interTiesSides = 0;
+        int interTiesGabel = 12;
+        int interTiesSides = 4;
         BillCalc instance = new BillCalc();
-        int expResult = 0;
+        int expResult = 32;
         int result = instance.angleBrackets(interTiesGabel, interTiesSides);
         assertEquals(expResult, result);
 
